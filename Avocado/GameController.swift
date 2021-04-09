8c07c55252eaed802703f9b3a25f24d6c0c274a3
Lucio Busa <dev.luciobusa@gmail.com>
2021-04-09 15:43:57 +0200

    new version

--- Avocado/GameController.swift
+++ Avocado/GameController.swift
@@ -17,18 +17,16 @@ class GameController: NSViewController {
     var showRight: Int!;
     var changeDegree: Int = 0;
     var number: Int!;
-    var toStart: Int!;
     var block: Int! = 0;
     let limit: Int! = 8;
-    var lecter: Int! = 0;
-    var dct1: [Character] = ["B","C","D","F","G","H","L","M","N","P","Q","R","S","T","V","Z"].shuffled()
-    var dct2: [Character] = ["B","C","D","F","G","H","L","M","N","P","Q","R","S","T","V","Z"].shuffled()
+    var phase: Int! = 0;
+    var dct: [String] = ["B","C","D","F","G","H","L","M","N","P","Q","R","S","T","V","Z"].shuffled()
     var monitor: Any?
     var optionsDictionary: [NSView.FullScreenModeOptionKey:Any]!
     
     @IBOutlet weak var message: NSTextField!
-    @IBOutlet weak var lecterRight: NSImageView!
-    @IBOutlet weak var lecterLeft: NSImageView!
+    @IBOutlet weak var lecterRight: NSTextField!
+    @IBOutlet weak var lecterLeft: NSTextField!
     @IBOutlet weak var cross: NSImageView!
     
     override func viewWillDisappear() {
@@ -37,30 +35,25 @@ class GameController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.monitor = NSEvent.addLocalMonitorForEvents(matching: .keyDown) {
+                   self.keyDown(with: $0)
+                   return nil
+               }
         begin();
     }
 
     
     func begin(){
-        self.toStart = 1;
         self.message.stringValue = "premi la barra spaziatrice"
-        
         self.message.isHidden = false;
         self.lecterLeft.isHidden = true;
         self.lecterRight.isHidden = true;
         self.cross.isHidden = true;
-        self.monitor = NSEvent.addLocalMonitorForEvents(matching: .keyDown) {
-                   self.keyDown(with: $0)
-                   return nil
-               }
     }
     
     func startGame(){
-        self.toStart = 0;
         self.showLeft = 0;
         self.showRight = 0;
-        self.message.isHidden = true;
-        self.cross.isHidden = false;
         initGame();
     }
     
@@ -70,15 +63,16 @@ class GameController: NSViewController {
             self.changeDegree = 200
         }
         view.window?.toggleFullScreen(self)
-        self.lecterLeft.frame.origin.x = CGFloat(cross.frame.origin.x - 200 - CGFloat(self.changeDegree))
-        self.lecterRight.frame.origin.x = CGFloat(cross.frame.origin.x + 200 + CGFloat(self.changeDegree))
-
         NSCursor.hide()
     
     }
     
     func initGame(){
-   
+        self.phase = 2;
+        self.message.isHidden = true;
+        self.cross.isHidden = false;
+        self.lecterRight.isHidden = true;
+        self.lecterLeft.isHidden = true;
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             if(self.showLeft < self.limit || self.showRight < self.limit){
             NSSound(named: "suono")?.play()
@@ -89,27 +83,47 @@ class GameController: NSViewController {
             }
             else {self.checkEnd()}
         }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+        self.begin()
+        }
     }
     
     func changeLecter(){
         self.number = Int.random(in: 0..<2)
         if( self.showLeft<self.limit) && (number == 0 || self.showRight == self.limit){
             self.number = 0;
+            if(self.block == 0){ self.lecterLeft.stringValue = dct[self.showLeft]}
+            else {self.lecterLeft.stringValue = dct[8+self.showLeft]}
+            self.lecterLeft.frame.origin.x = CGFloat(cross.frame.origin.x - 200 - CGFloat(self.changeDegree))
             self.lecterLeft.isHidden = false
+            showLeft += 1
         }
         else if(self.showRight<self.limit){
-            self.lecterRight.isHidden = false
             self.number = 1;
+            if(self.block == 0){ self.lecterRight.stringValue = dct[8+self.showRight]}
+            else {self.lecterRight.stringValue = dct[self.showRight]}
+            self.lecterRight.frame.origin.x = CGFloat(cross.frame.origin.x + 200 + CGFloat(self.changeDegree))
+            self.lecterRight.isHidden = false
+            showRight += 1
         }
-     
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             self.lecterLeft.isHidden = true;
             self.lecterRight.isHidden = true;
-            self.initGame()
         }
-
-      
-      
+    }
+    
+    func letterMiddle(){
+        self.message.isHidden = true;
+        self.cross.isHidden = true;
+        if(self.number==0){
+            self.lecterLeft.frame.origin.x = CGFloat(cross.frame.origin.x)
+            self.lecterLeft.isHidden = false;
+        }
+        else{
+            self.lecterRight.frame.origin.x = CGFloat(cross.frame.origin.x)
+            self.lecterRight.isHidden = false;
+        }
+        self.phase = 1
     }
     
     func checkEnd(){
@@ -129,17 +143,15 @@ class GameController: NSViewController {
         //controlla se la space bar è stata premuta
         switch event.keyCode {
         case 49:
-            if(self.toStart == 1) {self.startGame()}
-            else {
-            //calcola i tempi di risposta in milli secondi
-               
-            // scarta il valore
-            if(ms>=150 && ms<=700){
-                print(names[block]+String(number)+": "+String(Int(ms)))
-                dct[names[block]+String(number)]?.append(Int(ms));
-                if(number == 0){showLeft += 1}
-                else {showRight += 1}
- }
+            switch self.phase {
+            case 0:
+                self.startGame()
+            case 1:
+                self.initGame()
+            case 2:
+                self.letterMiddle()
+            default:
+                print("no")
             }
         case 53:
             //self.presentingViewController?.dismiss(self)
